1f8065fdb9b5d87306cea12df622237c8aa84114
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2023-11-09 16:34:40 +0900

    Title: 정수 명령 처리 8, Time: 313ms, Memory: 30MB, Status: Passed - CodeTree

--- 231109/정수 명령 처리 8/process-numeric-commands-8.py
+++ 231109/정수 명령 처리 8/process-numeric-commands-8.py
@@ -1,95 +1,72 @@
-# Node 클래스를 만들어줍니다.
 class Node:
     def __init__(self, data):
-        self.data = data 
-        self.next = None 
+        self.data = data
+        self.next = None
         self.prev = None
 
-
-# 이중 연결 리스트 클래스를 만들어줍니다.
-class DoublyLinkedList:
+class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-        self.node_num = 0
-  
-    def push_front(self, new_data):   # 원소를 첫 번째 위치에 넣어줍니다.
-        new_node = Node(new_data)     # 새로운 노드를 만들어줍니다.
-        new_node.next = self.head     # 새로운 노드의 next 값을 head로 바꿔줍니다.
+        self.size = 0
 
-        if self.head != None:         # 기존 리스트가 비어있지 않았다면
-            self.head.prev = new_node # 이전 head의 prev값을 바꾼 뒤
-            self.head = new_node      # head값을 변경해줍니다.
-            new_node.prev = None
-        else:                         # 기존 리스트가 비어있었다면
-            self.head = new_node      # head, tail을 새로 설정해줍니다.
+    def push_front(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.size += 1
+    
+    def push_back(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
             self.tail = new_node
-            new_node.prev = None
-        self.node_num += 1
+        self.size += 1
 
-    def push_back(self, new_data):    # 원소를 맨 끝 위치에 넣어줍니다.
-        new_node = Node(new_data)     # 새로운 노드를 만들어줍니다.
-        new_node.prev = self.tail     # 새로운 노드의 prev 값을 tail로 바꿔줍니다.
+    def pop_front(self):
+        if self.head:
+            value = self.head.data
+            self.head = self.head.next
+            if self.head:
+                self.head.prev = None
+            else:
+                self.tail = None
+            self.size -= 1
+            return value
+    
+    def pop_back(self):
+        if self.tail:
+            value = self.tail.data
+            self.tail = self.tail.prev
+            if self.tail:
+                self.tail.next = None
+            else:
+                self.head = None
+            self.size -= 1
+            return value
 
-        if self.tail != None:         # 기존 리스트가 비어있지 않았다면
-            self.tail.next = new_node # 이전 tail의 next값을 바꾼 뒤
-            self.tail = new_node      # tail 값을 변경해줍니다.
-            new_node.next = None
-        else:                         # 기존 리스트가 비어있었다면
-            self.head = new_node      # head, tail을 새로 설정해줍니다.
-            self.tail = new_node
-            new_node.next = None
-        self.node_num += 1
+    def get_size(self):
+        return self.size
+    
+    def is_empty(self):
+        return 1 if self.size == 0 else 0
 
-    def pop_front(self):              # 첫 번째 수를 빼면서 동시에 그 수를 반환합니다.
-        if self.head.next == None:  # 노드가 하나 남았다면
-            temp = self.head
-            self.head = None          # head값을 None으로 바꿔주고
-            self.tail = None          # tail값도 None으로 바꿔주고
-            self.node_num = 0         # 원소의 수도 0개로 변경해줍니다.
-            return temp.data
-        else:
-            temp = self.head
-            temp.next.prev = None     # 새로 head가 될 노드의 prev값을 지워줍니다.
-            self.head = temp.next     # head값을 새로 갱신해주고
-            temp.next = None          # 이전 head의 next 값을 지워줍니다.
-            self.node_num -= 1
-            return temp.data
-  
-    def pop_back(self):               # 맨 끝에 있는 수를 빼면서 동시에 그 수를 반환합니다.
-        if self.tail.prev == None:  # 노드가 하나 남았다면
-            temp = self.tail
-            self.head = None          # head값을 None으로 바꿔주고
-            self.tail = None          # tail값도 None으로 바꿔주고
-            self.node_num = 0         # 원소의 수도 0개로 변경해줍니다.
-            return temp.data
-        else:
-            temp = self.tail
-            temp.prev.next = None     # 새로 tail이 될 노드의 next값을 지워줍니다.
-            self.tail = temp.prev     # tail값을 새로 갱신해주고
-            temp.prev = None          # 이전 tail의 prev 값을 지워줍니다.
-            self.node_num -= 1
-            return temp.data
+    def get_front(self):
+        return self.head.data if self.head else None
 
-    def size(self):
-        return self.node_num
+    def get_back(self):
+        return self.tail.data if self.tail else None
 
-    def empty(self):
-        if self.size:
-            return 0
-        else:
-            return 1
-
-    def front(self):                  # 첫 번째 수를 반환합니다.
-        return self.head.data
-  
-    def back(self):                   # 맨 끝에 있는 수를 반환합니다.
-        return self.tail.data
-
-
-if __name__ == "__main__":
+def main():
     N = int(input())
-    sll = DoublyLinkedList()
+    sll = LinkedList()
 
     for _ in range(N):
         command = input().split()
@@ -103,10 +80,13 @@ if __name__ == "__main__":
         elif command[0] == "pop_back":
             print(sll.pop_back())
         elif command[0] == "size":
-            print(sll.size())
+            print(sll.get_size())
         elif command[0] == "empty":
-            print(sll.empty())
+            print(sll.is_empty())
         elif command[0] == "front":
-            print(sll.front())
+            print(sll.get_front())
         elif command[0] == "back":
-            print(sll.back())
+            print(sll.get_back())
+
+if __name__ == "__main__":
+    main()
