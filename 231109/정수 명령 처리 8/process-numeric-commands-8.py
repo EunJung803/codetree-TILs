99645c7af9363904dc34be31414f13bac9743d8d
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2023-11-09 16:28:32 +0900

    Title: 정수 명령 처리 8, Time: 416ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 231109/정수 명령 처리 8/process-numeric-commands-8.py
+++ 231109/정수 명령 처리 8/process-numeric-commands-8.py
@@ -1,32 +1,3 @@
-# from collections import deque
-
-# N = int(input())
-
-# ans = deque()
-
-# for i in range(N):
-#     arr = input().split()
-#     if(arr[0] == "push_front"):
-#         ans.appendleft(int(arr[1]))
-#     if(arr[0] == "push_back"):
-#         ans.append(int(arr[1]))
-#     if(arr[0] == "pop_front"):
-#         print(ans.popleft())
-#     if(arr[0] == "pop_back"):
-#         print(ans.pop())
-#     if(arr[0] == "size"):
-#         print(len(ans))
-#     if(arr[0] == "empty"):
-#         if(ans):
-#             print(0)
-#         else:
-#             print(1)
-#     if(arr[0] == "front"):
-#         print(ans[0])
-#     if(arr[0] == "back"):
-#         print(ans[-1])
-        
-
 # Node 클래스를 만들어줍니다.
 class Node:
     def __init__(self, data):
@@ -50,7 +21,6 @@ class DoublyLinkedList:
             self.head.prev = new_node # 이전 head의 prev값을 바꾼 뒤
             self.head = new_node      # head값을 변경해줍니다.
             new_node.prev = None
-    
         else:                         # 기존 리스트가 비어있었다면
             self.head = new_node      # head, tail을 새로 설정해줍니다.
             self.tail = new_node
@@ -66,7 +36,6 @@ class DoublyLinkedList:
             self.tail.next = new_node # 이전 tail의 next값을 바꾼 뒤
             self.tail = new_node      # tail 값을 변경해줍니다.
             new_node.next = None
-
         else:                         # 기존 리스트가 비어있었다면
             self.head = new_node      # head, tail을 새로 설정해줍니다.
             self.tail = new_node
@@ -75,17 +44,12 @@ class DoublyLinkedList:
         self.node_num += 1
 
     def pop_front(self):              # 첫 번째 수를 빼면서 동시에 그 수를 반환합니다.
-        if self.head == None:
-            print("List is empty")
-    
-        elif self.head.next == None:  # 노드가 하나 남았다면
+        if self.head.next == None:  # 노드가 하나 남았다면
             temp = self.head
-
             self.head = None          # head값을 None으로 바꿔주고
             self.tail = None          # tail값도 None으로 바꿔주고
             self.node_num = 0         # 원소의 수도 0개로 변경해줍니다.
             return temp.data
-
         else:
             temp = self.head
             temp.next.prev = None     # 새로 head가 될 노드의 prev값을 지워줍니다.
@@ -96,17 +60,12 @@ class DoublyLinkedList:
             return temp.data
   
     def pop_back(self):               # 맨 끝에 있는 수를 빼면서 동시에 그 수를 반환합니다.
-        if self.tail == None:
-            print("List is empty")
-
-        elif self.tail.prev == None:  # 노드가 하나 남았다면
+        if self.tail.prev == None:  # 노드가 하나 남았다면
             temp = self.tail
-
             self.head = None          # head값을 None으로 바꿔주고
             self.tail = None          # tail값도 None으로 바꿔주고
             self.node_num = 0         # 원소의 수도 0개로 변경해줍니다.
             return temp.data
-
         else:
             temp = self.tail
             temp.prev.next = None     # 새로 tail이 될 노드의 next값을 지워줍니다.
