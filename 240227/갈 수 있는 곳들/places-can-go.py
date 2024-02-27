0b0bbf9c449f89b9b74bb9d993ff8ff77cda6ca1
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-02-27 12:08:45 +0900

    Title: 갈 수 있는 곳들, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240227/갈 수 있는 곳들/places-can-go.py
+++ 240227/갈 수 있는 곳들/places-can-go.py
@@ -1,8 +1,8 @@
 n, k = map(int, input().split())
-
 matrix = [list(map(int, input().split())) for _ in range(n)]
+start_point = [list(map(int, input().split())) for _ in range(k)]
 
-start_point = [list(map(int, input().split())) for _ in range(k)] 
+visited = [list(0 for _ in range(n)) for _ in range(n)]
 
 count = 0
 
@@ -17,14 +17,11 @@ def can_go(x, y):
 while(start_point):
     start = start_point.pop(0)
 
-    visited = [list(0 for _ in range(n)) for _ in range(n)]
-
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
 
     x = start[0]-1
     y = start[1]-1
-    count += 1      # 시작점 카운트
 
     for i in range(4):
         new_x = x + dx[i]
@@ -33,6 +30,7 @@ while(start_point):
         if(can_go(new_x, new_y)):
             visited[new_x][new_y] = 1
             count += 1
+            start_point.append([new_x + 1, new_y + 1])
 
     # print(count)
 
