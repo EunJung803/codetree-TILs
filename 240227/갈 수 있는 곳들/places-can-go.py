08026f526ba43c9c85098539a6eaaec2956a1f83
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-02-27 12:13:30 +0900

    Title: 갈 수 있는 곳들, Time: 227ms, Memory: 26MB, Status: Passed - CodeTree

--- 240227/갈 수 있는 곳들/places-can-go.py
+++ 240227/갈 수 있는 곳들/places-can-go.py
@@ -17,21 +17,20 @@ def can_go(x, y):
 while(start_point):
     start = start_point.pop(0)
 
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
+    dx = [0, 0, 1, 0, -1]
+    dy = [0, 1, 0, -1, 0]
 
     x = start[0]-1
     y = start[1]-1
 
-    for i in range(4):
+    for i in range(5):
         new_x = x + dx[i]
         new_y = y + dy[i]
 
         if(can_go(new_x, new_y)):
             visited[new_x][new_y] = 1
-            count += 1
             start_point.append([new_x + 1, new_y + 1])
-
+            count += 1
     # print(count)
 
 print(count)
