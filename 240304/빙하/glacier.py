583775491c8ee7ef17b3354d0c6b38bde977417c
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-04 03:30:56 +0900

    Title: 빙하, Time: 2603ms, Memory: 37MB, Status: Passed - CodeTree

--- 240304/빙하/glacier.py
+++ 240304/빙하/glacier.py
@@ -82,6 +82,8 @@ def bfs_ice_melt(q):
             if(can_go_ice(new_x, new_y)):
                 if(check_water(new_x, new_y)):   # 주변에 빙하를 녹일 수 있는 물과 맞닿아있는지 확인
                     ice_visited[new_x][new_y] = 1
+                    # if(matrix[new_x][new_y] == 1):
+                    #     melting.append((new_x, new_y))
                     melting.append((new_x, new_y))
                     q.append((new_x, new_y))
 
@@ -124,11 +126,14 @@ while not (all_melt(matrix)):
             last_ice = 0
             for m in range(len(melting)):
                 ice = melting[m]
-                if (matrix[ice[0]][ice[1]] == 1):
+                if(matrix[ice[0]][ice[1]] == 1):
                     matrix[ice[0]][ice[1]] = 0
                     last_ice += 1
-                outer_water.add((ice[0], ice[1]))  # 녹았으니까 외부 물에 추가해주기
+
+                outer_water.add((ice[0], ice[1]))   # 녹았으니까 외부 물에 추가해주기
                 water_visited[ice[0]][ice[1]] = 1
                 ice_visited[ice[0]][ice[1]] = 0
+                q.append((ice[0], ice[1]))
+                bfs_outer_water(q)
 
 print(T, last_ice)
