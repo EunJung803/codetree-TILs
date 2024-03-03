6a859b7a856dc3d1331fac1e9d585dfca8703f14
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-03 22:40:50 +0900

    Title: 빙하, Time: 199ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240303/빙하/glacier.py
@@ -0,0 +1,114 @@
+from collections import deque
+
+N, M = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+
+# 범위 체크
+def check_range(x, y):
+    if (x < 0 or y < 0 or x >= N or y >= M):
+        return False
+    else:
+        return True
+
+# 범위를 벗어나지 않고, 방문하지 않은 곳의 물인지 체크
+water_visited = [list(0 for _ in range(M)) for _ in range(N)]
+def can_go_water(x, y):
+    if (check_range(x, y) == False):
+        return False
+    if (matrix[x][y] == 1 or water_visited[x][y] == 1):
+        return False
+    else:
+        return True
+
+# 범위를 벗어나지 않고, 방문하지 않은 곳의 빙하인지 체크
+ice_visited = [list(0 for _ in range(M)) for _ in range(N)]
+def can_go_ice(x, y):
+    if (check_range(x, y) == False):
+        return False
+    if (ice_visited[x][y] == 1):
+        return False
+    else:
+        return True
+
+q = deque()
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+# 외부를 둘러싸는 물 찾기
+outer_water = []
+def bfs_outer_water(q):
+    while(q):
+        curr = q.popleft()
+        x = curr[0]
+        y = curr[1]
+
+        for i in range(4):
+            new_x = x + dx[i]
+            new_y = y + dy[i]
+            if(can_go_water(new_x, new_y)):
+                water_visited[new_x][new_y] = 1
+                outer_water.append((new_x, new_y))
+                q.append((new_x, new_y))
+
+# 맨 처음 외부와 둘러쌓인 물 구하기
+water_visited[0][0] = 1
+outer_water.append((0, 0))
+q.append((0, 0))
+
+bfs_outer_water(q)
+
+# 외부와 맞닿은 물이 주변에 있는지 확인
+def check_water(x, y):
+    for i in range(4):
+        n_x = x + dx[i]
+        n_y = y + dy[i]
+
+        if(check_range(n_x, n_y) and matrix[n_x][n_y] == 0):
+            if((n_x, n_y) in outer_water):
+                return True
+    return False
+
+
+# 녹는 얼음 찾기
+melting = []
+def bfs_ice_melt(q):
+    while(q):
+        curr = q.popleft()
+        x = curr[0]
+        y = curr[1]
+
+        for i in range(4):
+            new_x = x + dx[i]
+            new_y = y + dy[i]
+            if(can_go_ice(new_x, new_y)):
+                if(check_water(new_x, new_y)):   # 주변에 빙하를 녹일 수 있는 물과 맞닿아있는지 확인
+                    ice_visited[new_x][new_y] = 1
+                    melting.append((new_x, new_y))
+                    q.append((new_x, new_y))
+
+T = 0
+last_ice = 0
+while(len(outer_water) < N * M):
+    for i in range(N):
+        for j in range(M):
+            melting = []
+
+            if(matrix[i][j] == 1 and ice_visited[i][j] == 0):
+                ice_visited[i][j] = 1
+                melting.append((i ,j))
+                q.append((i, j))
+
+                bfs_ice_melt(q)
+
+            if(melting):
+                T += 1
+                last_ice = 0
+                for m in range(len(melting)):
+                    ice = melting[m]
+                    if(matrix[ice[0]][ice[1]] == 1):
+                        matrix[ice[0]][ice[1]] = 0
+                        last_ice += 1
+                    outer_water.append((ice[0], ice[1]))        # 녹았으니까 외부 물에 추가해주기
+
+print(T, last_ice)
