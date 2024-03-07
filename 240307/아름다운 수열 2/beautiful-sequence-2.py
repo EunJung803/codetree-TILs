e4636e14276d18cbd4298b5c0151d1a69aad4910
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-07 14:55:18 +0900

    Title: 아름다운 수열 2, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240307/아름다운 수열 2/beautiful-sequence-2.py
+++ 240307/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,12 +1,8 @@
-from itertools import permutations
-
 N, M = map(int, input().split())
 seq_A = list(map(int, input().split()))
 seq_B = list(map(int, input().split()))
 
 cnt = 0
-
-beautiful_seq = list(permutations(seq_B))
 
 for i in range(N):
     flag = True
@@ -14,8 +10,12 @@ for i in range(N):
     for j in range(i, i+M):
         if(i+M <= N):
             sub_str.append(seq_A[j])
-
-    if(tuple(sub_str) in beautiful_seq):
-        cnt += 1
+    if(sub_str):
+        for j in range(M):
+            if(sub_str[j] not in seq_B):
+                flag = False
+                break
+        if(flag):
+            cnt += 1
 
 print(cnt)
