ab61f7c7526b523fbfa4ea7a5b931201749c0401
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-07 22:24:28 +0900

    Title: 아름다운 수열 2, Time: 1323ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240307/아름다운 수열 2/beautiful-sequence-2.py
+++ 240307/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,17 +1,26 @@
-N, M = map(int, input().split())
-seq_A = list(map(int, input().split()))
-seq_B = list(map(int, input().split()))
+from itertools import permutations
 
-cnt = 0
+def make_b_case(B):
+    b = list(set(list(permutations(B,len(B)))))
+    #b_case = []
+    #for i in range(len(B)):
+        #b_case.append(B[i:]+B[:i])
+    #return b_case
+    return b
 
-for i in range(N):
-    flag = True
-    sub_str = []
-    for j in range(i, i+M):
-        if(i+M <= N):
-            sub_str.append(seq_A[j])
-    if(sub_str):
-        if(sorted(sub_str) == sorted(seq_B)):
-            cnt += 1
+def beautiful_num(A,B):
+    num = 0
+    for bn in range(len(A)):
+        if A[bn:bn+len(B)]==list(B):
+            num += 1
+    return num
 
-print(cnt)
+N,M = map(int,input().split())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+answer = 0
+b_case = make_b_case(B)
+for i in range(len(b_case)):
+    answer += beautiful_num(A,b_case[i])
+print(answer)
