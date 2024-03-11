d1e70f62b9b9afc4ce04c54d5aa5af866a60eb7d
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-11 23:38:04 +0900

    Title: 금 채굴하기, Time: 430ms, Memory: 29MB, Status: Passed - CodeTree

--- 240311/금 채굴하기/gold-mining.py
+++ 240311/금 채굴하기/gold-mining.py
@@ -1,39 +1,36 @@
-from collections import deque
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-def in_range(x,y):
-    if 0<=x<N and 0<=y<N:
-        return True
-    return False
 
-def bfs(i,j,size):
-    cost,earn = (size*size)+((size+1)*(size+1)),0
-    q = deque()
-    dx,dy = [-1,1,0,0],[0,0,-1,1]
-    isVisited = [[-1 for ii in range(N)] for jj in range(N)]
-    isVisited[i][j] = 0
-    q.append((i,j))
-    while q:
-        x,y = q.popleft()
-        if isVisited[x][y]>size:
-            break
-        if isVisited[x][y]<=size and grid[x][y] == 1:
-            earn += 1
-        for d in range(4):
-            nx,ny = x+dx[d],y+dy[d]
-            if in_range(nx,ny) and isVisited[nx][ny]==-1:
-                isVisited[nx][ny] = isVisited[x][y]+1
-                q.append((nx,ny))
-    return [cost,earn]
+# 주어진 k에 대하여 마름모의 넓이를 반환합니다.
+def get_area(k):
+    return k * k + (k + 1) * (k + 1)
 
-###############################################
-N,M = map(int,input().split())
-grid = [list(map(int,input().split())) for _ in range(N)]
-answer = 0
 
-for x in range(N):
-    for y in range(N):
-        for size in range(N+1):
-            cost,num = bfs(x,y,size)
-            if num*M - cost >= 0:
-                answer = max(answer,num)
-print(answer)
+# 주어진 k에 대하여 채굴 가능한 금의 개수를 반환합니다.
+def get_num_of_gold(row, col, k):
+    return sum([
+        grid[i][j]
+        for i in range(n)
+        for j in range(n)
+        if abs(row - i) + abs(col - j) <= k
+    ])
+
+
+max_gold = 0
+
+# 격자의 각 위치가 마름모의 중앙일 때 채굴 가능한 금의 개수를 구합니다.
+for row in range(n):
+    for col in range(n):
+        for k in range(2 * (n - 1) + 1):
+            num_of_gold = get_num_of_gold(row, col, k)
+            
+            # 손해를 보지 않으면서 채굴할 수 있는 최대 금의 개수를 저장합니다.
+            if num_of_gold * m >= get_area(k):
+                max_gold = max(max_gold, num_of_gold)
+
+print(max_gold)
