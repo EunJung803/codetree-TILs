c6fd781c224925a506c09e200e70552c08d92561
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-13 02:07:26 +0900

    Title: 상해버린 치즈, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240313/상해버린 치즈/rotten-cheese.py
+++ 240313/상해버린 치즈/rotten-cheese.py
@@ -6,24 +6,24 @@ med = 0
 
 check_human = [False for _ in range(N)]
 
-def find_other(bad_cheese, med, check_human):
+def find_other(bad_cheese, check_human):
     for b in range(D):
         if(cheese_list[b][1] == bad_cheese and check_human[cheese_list[b][0]-1] == False):
-            med += 1
             check_human[cheese_list[b][0]-1] = True
-    return med
 
 for i in range(S):
     sick_person = sick_list[i][0]
     t = sick_list[i][1]
 
     for j in range(D):
-        if(cheese_list[j][0] == sick_person and cheese_list[j][2] < t):
-            med += 1
+        if(cheese_list[j][0] == sick_person and cheese_list[j][2] < t and check_human[sick_person-1] == False):
             check_human[sick_person-1] = True
+            tmp = check_human.count(True)
 
-            med = find_other(cheese_list[j][1], med, check_human)       # 추가로 아플 가능성이 있는 사람을 찾아내기
-        if(med == N):
-            break
+            find_other(cheese_list[j][1], check_human)       # 추가로 아플 가능성이 있는 사람을 찾아내기
+            if(tmp == check_human.count(True)):
+                check_human[sick_person - 1] = False
+                continue
 
+# print(check_human)
 print(check_human.count(True))
