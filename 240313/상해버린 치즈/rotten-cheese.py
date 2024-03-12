e0bb5ba3010b7a61ee0149c7236570320cbeb887
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-13 01:50:44 +0900

    Title: 상해버린 치즈, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240313/상해버린 치즈/rotten-cheese.py
@@ -0,0 +1,29 @@
+N, M, D, S = map(int, input().split())
+cheese_list = [list(map(int, input().split())) for _ in range(D)]
+sick_list = [list(map(int, input().split())) for _ in range(S)]
+
+med = 0
+
+check_human = [False for _ in range(N)]
+
+def find_other(bad_cheese, med, check_human):
+    for b in range(D):
+        if(cheese_list[b][1] == bad_cheese and check_human[cheese_list[b][0]-1] == False):
+            med += 1
+            check_human[cheese_list[b][0]-1] = True
+    return med
+
+for i in range(S):
+    sick_person = sick_list[i][0]
+    t = sick_list[i][1]
+
+    for j in range(D):
+        if(cheese_list[j][0] == sick_person and cheese_list[j][2] <= t and check_human[sick_person-1] == False):
+            med += 1
+            check_human[sick_person-1] = True
+
+            med = find_other(cheese_list[j][1], med, check_human)
+        if(med == N):
+            break
+
+print(med)
