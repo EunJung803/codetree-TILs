db959ddd33fceb91dc7af5e0d227978a34290de3
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-13 00:35:43 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240313/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -0,0 +1,23 @@
+N = int(input())
+str_input = input()
+str_list = list(str_input)
+
+checked = set()
+
+double = []
+
+cnt = 1
+ans = 0
+
+while(cnt < N):
+    for i in range(N):
+        for j in range(i, N):
+            sub_str = str_input[i:j+1]
+
+            if(sub_str not in checked):
+                checked.add(sub_str)
+            else:
+                cnt += 1
+                ans = max(ans, len(sub_str)+1)
+
+print(ans)
