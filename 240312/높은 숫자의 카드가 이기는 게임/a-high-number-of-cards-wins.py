a597c990fdb7b430aa132e988580bdab552ec02f
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-12 22:32:37 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 252ms, Memory: 33MB, Status: Passed - CodeTree

--- 240312/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240312/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -3,12 +3,26 @@ B_cards = list(int(input()) for _ in range(N))
 
 all_cards = list(i for i in range(1, 2*N+1))
 
+B_cards.sort()
+
 A_cards = []
+
+# b = 0
+# for i in range(2*N):
+#     if(len(A_cards) == N or b >= N):
+#         break
+#     if(all_cards[i] == B_cards[b] and b < N):
+#         b += 1
+#     else:
+#         A_cards.append(all_cards[i])
+#         continue
+
+b_set = set(B_cards)        # in 연산의 시간초과 해결을 위하여 set로 변경
+
 for i in range(len(all_cards)):
-    if(all_cards[i] not in B_cards):
+    if(all_cards[i] not in b_set):
         A_cards.append(all_cards[i])
 
-B_cards.sort()
 A_cards.sort()
 
 ans = 0
