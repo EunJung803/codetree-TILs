e1fc5ec284cd55e18cb0e2e5ebaa922ba6b9bac1
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-15 17:38:10 +0900

    Title: 그래프 탐색, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240315/그래프 탐색/graph-traversal.py
@@ -0,0 +1,22 @@
+def dfs(point):
+    global cnt
+    for np in grid_list[point]:
+        if visited[np]==False:
+            cnt += 1
+            visited[np] = True
+            dfs(np)
+
+
+N,M = map(int,input().split())
+grid_list = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    s,e = map(int,input().split())
+    grid_list[s].append(e)
+    grid_list[e].append(s)
+
+visited = [False for _ in range(N)]
+visited[1] = True
+cnt = 0
+dfs(1)
+print(cnt)
