9df7ab1481a97b50c6e620705ea50442e79667e5
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-16 14:45:35 +0900

    Title: 사각형 채우기 3, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240316/사각형 채우기 3/rectangle-fill-3.py
+++ 240316/사각형 채우기 3/rectangle-fill-3.py
@@ -1,8 +1,7 @@
 n = int(input())
 dp = [-1 for _ in range(1001)]
-dp[0]=0
 dp[1]=2
 dp[2]=7
 for i in range(3,n+1):
-    dp[i]=dp[i-1]*2 + dp[i-2]*3
+    dp[i]=dp[i-1]*2 + dp[i-2]*4
 print(dp[n]%1000000007)
