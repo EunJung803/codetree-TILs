6f4ea15e9d04d83d0f9199d325b3083d8e073734
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-20 00:04:36 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 726ms, Memory: 74MB, Status: Passed - CodeTree

--- 240320/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240320/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -6,27 +6,27 @@ dp = [[-1 for i in range(n)] for j in range(n)]
 dxs,dys = [-1,1,0,0],[0,0,-1,1]
 
 q = deque()
-visited = [[-1 for i in range(n)] for j in range(n)]
+visited = [[0 for i in range(n)] for j in range(n)]
 
 def bfs():
     while q:
         x,y = q.popleft()
         for dx,dy in zip(dxs,dys):
             nx,ny = x+dx,y+dy
-            if (0<=nx<n and 0<=ny<n) and grid[nx][ny]>grid[x][y] and visited[nx][ny]==-1:
-                visited[nx][ny] = visited[x][y]+1
+            if (0<=nx<n and 0<=ny<n) and grid[nx][ny]>grid[x][y] and visited[nx][ny]==0:
+                visited[nx][ny] = 1
                 q.append((nx,ny))
 
 def initialize():
     #dp[0][0]값 구하기
     q.append((0,0))
-    visited[0][0] = 0
+    visited[0][0] = 1
     bfs()
-    dp[0][0] = max([
+    dp[0][0] = sum([
         visited[i][j]
         for i in range(n)
         for j in range(n)
-    ])
+    ])-1
 
 def getdp(x,y):
     if dp[x][y] == -1:
@@ -42,6 +42,7 @@ initialize()
 for i in range(n):
     for j in range(n):
         getdp(i,j)
+
 print(max([
     dp[i][j]
     for i in range(n)
