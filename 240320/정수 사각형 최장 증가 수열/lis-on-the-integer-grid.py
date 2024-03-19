c0b5431282e00a1bd08852d84b5979236267522e
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-20 00:00:15 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 552ms, Memory: 76MB, Status: Wrong Answer - CodeTree

---
+++ 240320/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+n = int(input())
+grid = [list(map(int,input().split())) for _ in range(n)]
+dp = [[-1 for i in range(n)] for j in range(n)]
+dxs,dys = [-1,1,0,0],[0,0,-1,1]
+
+q = deque()
+visited = [[-1 for i in range(n)] for j in range(n)]
+
+def bfs():
+    while q:
+        x,y = q.popleft()
+        for dx,dy in zip(dxs,dys):
+            nx,ny = x+dx,y+dy
+            if (0<=nx<n and 0<=ny<n) and grid[nx][ny]>grid[x][y] and visited[nx][ny]==-1:
+                visited[nx][ny] = visited[x][y]+1
+                q.append((nx,ny))
+
+def initialize():
+    #dp[0][0]값 구하기
+    q.append((0,0))
+    visited[0][0] = 0
+    bfs()
+    dp[0][0] = max([
+        visited[i][j]
+        for i in range(n)
+        for j in range(n)
+    ])
+
+def getdp(x,y):
+    if dp[x][y] == -1:
+        dp[x][y] = 0
+        for dx,dy in zip(dxs,dys):
+            nx,ny = x+dx,y+dy
+            if (0<=nx<n and 0<=ny<n) and grid[nx][ny]>grid[x][y]:
+                dp[x][y] = max(dp[x][y],getdp(nx,ny)+1)
+
+    return dp[x][y]
+
+initialize()
+for i in range(n):
+    for j in range(n):
+        getdp(i,j)
+print(max([
+    dp[i][j]
+    for i in range(n)
+    for j in range(n)
+])+1)
