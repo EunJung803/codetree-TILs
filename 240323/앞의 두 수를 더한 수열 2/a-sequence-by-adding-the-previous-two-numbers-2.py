923fccc9689f7994d6be8a53281b557b1d80fcab
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-03-23 00:43:29 +0900

    Title: 앞의 두 수를 더한 수열 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240323/앞의 두 수를 더한 수열 2/a-sequence-by-adding-the-previous-two-numbers-2.py
+++ 240323/앞의 두 수를 더한 수열 2/a-sequence-by-adding-the-previous-two-numbers-2.py
@@ -1,9 +1,13 @@
 n = int(input())
 
-dp = [0 for _ in range(n)]
-dp[0], dp[1] = 1, 1
+if(n >= 2):
+    dp = [0 for _ in range(n)]
+    dp[0], dp[1] = 1, 1
 
-for i in range(2, n):
-    dp[i] = dp[i-1] + dp[i-2]
+    for i in range(2, n):
+        dp[i] = dp[i-1] + dp[i-2]
 
-print(dp[n-1])
+    print(dp[n-1])
+
+else:
+    print(1)
